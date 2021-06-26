82d53e9db9132fc2bb826f5cc4c13c6a0a72dc59
zeguilherme99 <zeguilherme008@hotmail.com>
2021-06-25 21:35:17 -0300

    Implementando o operador Nao

--- src/OperadorNao.java
+++ src/OperadorNao.java
@@ -10,7 +10,9 @@ public class OperadorNao {
 		tipoUsuario = leitor.next();
 		
 		//Aqui é preciso criar um if que verifique se o tipo de usuário digitado é DIFERENTE de "administrador"
-		
+		if(!tipoUsuario.equalsIgnoreCase("administrador")) {
+			System.out.println("Atenção, usuário sem poderes administrativos. Algumas tarefas serão bloqueadas");
+		}
 		leitor.close();
 		
 
